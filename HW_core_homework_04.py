66f7da1b8d2dd853d9427b08bbc696de6cd1627b
newxata <9119355@gmail.com>
2024-12-29 15:02:30 +0200

    Finally

--- HW_core_homework_04.py
+++ HW_core_homework_04.py
@@ -31,5 +31,31 @@ total, average = total_salary('path/to/salary_file.txt')
 print(f'Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}')
 
 
+
 # Excesize 2
 
+def get_cats_info(path):
+    # Створюємо пустий список котів
+    cats_list = []
+    try:
+        # Читаємо файл з данними у відповідному кодуванні
+        with open(path, 'r', encoding='utf-8') as file:
+            for line in file:
+                # Розділяємо прочитанні значення за комами
+                id, name, age = line.strip().split(',')
+                # Присвоюємо отриманні значення відповідному ключу списку
+                cats_list.append({
+                    'id': id,
+                    'name': name,
+                    'age': age
+                })
+    # Додаємо відповідні виключення 
+    except ValueError:
+        print(f'Рядок не відповідає формату: {line.strip()}')
+    except FileNotFoundError:
+        print(f'Файл не знайдено!')
+
+    return cats_list
+
+cats_info = get_cats_info("path/to/cats_file.txt")
+print(cats_info)
